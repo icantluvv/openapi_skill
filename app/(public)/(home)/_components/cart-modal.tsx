5cb20c50deac0a92a7f635639fb56cdbedd96087
PanteleevSergey <s.panteleev@webpractik.ru>
2025-07-02 10:16:47 +0300

    fix(cart-modal): исправил закрытие модального окна корзины по нажатию вне модального окна

--- app/(public)/(home)/_components/cart-modal.tsx
+++ app/(public)/(home)/_components/cart-modal.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Button } from '@repo/core/button';
 import { Typography } from '@repo/core/typography';
 import { useModalBackground } from '#/hooks/use-modal-background';
+import { useEffect, useRef } from 'react';
 
 import CartHead from '@/_shared/cart/cart-head';
 import CartForm from '@/_shared/cart/form/cart-form';
@@ -11,20 +11,38 @@ import { useCartStore } from '@/store/cart-store';
 
 function CartModal() {
     const { items, setShowCart, showCart, totalPrice } = useCartStore();
+    const modalRef = useRef<HTMLDivElement>(null);
 
     useModalBackground();
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+                setShowCart();
+            }
+        }
+
+        if (showCart) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [showCart, setShowCart]);
+
     if (!showCart) {
         return null;
     }
 
-    function closeCart() {
-        setShowCart();
-    }
-
     return (
         <div className="no-scrollbar fixed inset-0 z-[9999] flex items-start justify-center overflow-y-auto bg-black/90 pt-[calc(50px+env(safe-area-inset-top))] pb-[50px] xl:pt-[calc(100px+env(safe-area-inset-top))] xl:pb-[100px]">
-            <Button className={'absolute z-[9000] size-full'} onClick={closeCart} />
-            <div className="z-[10000] flex w-[90vw] flex-col gap-[20px] overflow-x-hidden overflow-y-auto bg-background px-[15px] py-[25] xl:w-full xl:max-w-[70vw] xl:gap-[35px] xl:px-[30px] xl:py-[50px] 2xl:max-w-[36.4vw]">
+            <div
+                className="z-[10000] flex w-[90vw] flex-col gap-[20px] overflow-x-hidden overflow-y-auto bg-background px-[15px] py-[25] xl:w-full xl:max-w-[70vw] xl:gap-[35px] xl:px-[30px] xl:py-[50px] 2xl:max-w-[36.4vw]"
+                ref={modalRef}
+            >
                 <CartHead />
 
                 <div className="flex flex-col gap-[8px]">
