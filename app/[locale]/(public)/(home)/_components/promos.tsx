a9fdfd0124d7b41c668dfb2e9559ba2262ef847d
Sergey <spanteleev670@gmail.com>
2026-01-25 17:05:04 +0300

    feat(promo): добавил параллакс эффект для блока

--- app/[locale]/(public)/(home)/_components/promos.tsx
+++ app/[locale]/(public)/(home)/_components/promos.tsx
@@ -1,3 +1,4 @@
+import Parallax from '@/_shared/ParallaxItem';
 import MobileSlider from '@/_shared/promo/mobile-slider';
 import Promo from '@/_shared/promo/promo';
 import getPromo from '~/api/services/products/get-all-promo';
@@ -7,18 +8,20 @@ async function Promos() {
 
     return (
         <section className="bg-background mb-[60px] mt-[60px] w-full md:mb-[85px] md:mt-[0px] xl:mb-[105px]">
-            <div className="no-scrollbar container overflow-x-auto">
-                <div className="flex hidden w-max gap-[32px] md:flex md:w-full">
-                    {promos.map(promo => (
-                        <Promo
-                            description={promo.description}
-                            key={promo.id}
-                            src={promo.image}
-                            title={promo.title}
-                        />
-                    ))}
+            <Parallax offset={-200}>
+                <div className="no-scrollbar container transform-gpu overflow-x-auto will-change-transform">
+                    <div className="hidden w-max gap-[32px] md:flex md:w-full">
+                        {promos.map(promo => (
+                            <Promo
+                                description={promo.description}
+                                key={promo.id}
+                                src={promo.image}
+                                title={promo.title}
+                            />
+                        ))}
+                    </div>
                 </div>
-            </div>
+            </Parallax>
             <MobileSlider items={promos} />
         </section>
     );
---
+++ app/_shared/ParallaxItem/parallax-item.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { type ReactNode, useRef } from 'react';
+
+type ParallaxProps = {
+    children: ReactNode;
+    className?: string;
+    offset?: number;
+};
+
+export default function Parallax({ children, className = '', offset = 50 }: ParallaxProps) {
+    const ref = useRef<HTMLDivElement>(null);
+
+    const { scrollYProgress } = useScroll({
+        offset: ['start end', 'end start'],
+        target: ref,
+    });
+
+    const y = useTransform(scrollYProgress, [0, 0.5], [-offset, 0]);
+
+    return (
+        // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+        <motion.div className={className} ref={ref} style={{ y }}>
+            {children}
+        </motion.div>
+    );
+}
